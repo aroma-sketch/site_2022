141a3f8f7b1d9c6b21d78925e7336d30432f295b
AROMA GUPTA <aroma.dev@codeoffun.com>
2022-09-19 19:38:09 +0530

    footer done

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./Footer.scss"
 import { useEffect, useState } from "react";
+import {AiOutlineEye,AiOutlineCopyrightCircle} from 'react-icons/ai';
 
  
 const Footer = () => {
+    const [count, setCount] = useState(null);
+    useEffect(() => {
+      fetch("https://api.countapi.xyz/update/aromagupta/mysite/?amount=1")
+        .then((res) => res.json())
+        .then((json) => setCount(json.value));
+    }, []);
 
     let getYear= () => {
         let currentYear = new Date().getFullYear();
@@ -13,17 +20,28 @@ const Footer = () => {
     
     return (
         <div className="footer">
-        
-            {/* ................Copyright area .......................*/}
-            <p> Build &amp; Developed by AromaGupta 
-            <br />
-            {/* ................githubstars & fork stats .............*/}
+<div className="center-part">
+        <div className="first-line">
+             {/* ................Copyright area .......................*/}
+            <p>Build &amp; Developed by AromaGupta</p>
+        </div>
+          
 
-            <br />
-             Copyright &copy; <span> {getYear()} </span> All Rights Reserved.
-           </p>
+            <div className="second-line">
+ {/* ................githubstars & fork stats ...*/}
+ <span>
+    <div className="ded">
+ <AiOutlineEye/>
+      {count && <h3> {count} </h3>} 
+      </div>
+ </span>
+            </div>
 
-        
+            <div className="third-line">
+                <p>Copyright&copy; <span> {getYear()} </span> All Rights Reserved.</p>
+            </div>
+            
+            </div>
         </div>
     )
 }
